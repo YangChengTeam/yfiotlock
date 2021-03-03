acc3cf9dbe68da44863bfbf25052c789382b3e6a
ckun9393 <caokun9393@163.com>
2021-03-03 17:05:12 +0800

    merge

--- app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/remote/VisitorManageActivity.java
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/remote/VisitorManageActivity.java
@@ -1,4 +1,16 @@
 package com.yc.yfiotlock.controller.activitys.lock.remote;
 
-public class VisitorManageActivity {
+import com.yc.yfiotlock.R;
+import com.yc.yfiotlock.controller.activitys.base.BaseActivity;
+
+public class VisitorManageActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_visitor_manage;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
 }
