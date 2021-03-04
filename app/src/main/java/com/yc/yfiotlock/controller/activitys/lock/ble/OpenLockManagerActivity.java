8fa19ec0ca5f9148db472725d23fad598eab7730
mzpbvsig <972403325@qq.com>
2021-03-04 15:00:18 +0800

    add lock index activity

--- app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/OpenLockManagerActivity.java
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/OpenLockManagerActivity.java
@@ -1,4 +1,16 @@
 package com.yc.yfiotlock.controller.activitys.lock.ble;
 
-public class OpenLockManagerActivity {
+import com.yc.yfiotlock.R;
+import com.yc.yfiotlock.controller.activitys.base.BaseActivity;
+
+public class OpenLockManagerActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.lock_ble_activity_open_lock_manager;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
 }
