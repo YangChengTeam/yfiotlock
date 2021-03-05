cb009bed7d97c7ad932f48df5b7b95301b006ac4
Dullyoung <664846453@qq.com>
2021-03-05 10:19:42 +0800

    finish ble_setting  deviceinfo  EditDeviceName add proguard-rules.pro finish UpdateIcon dialog

--- app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/LockSettingActivity.java
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/LockSettingActivity.java
@@ -1,11 +1,74 @@
 package com.yc.yfiotlock.controller.activitys.lock.ble;
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.yc.yfiotlock.R;
 import com.yc.yfiotlock.controller.activitys.base.BaseBackActivity;
+import com.yc.yfiotlock.model.bean.SettingInfo;
+import com.yc.yfiotlock.utils.CommonUtils;
+import com.yc.yfiotlock.view.adapters.SettingAdapter;
+import com.yc.yfiotlock.view.widgets.SettingSoundView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LockSettingActivity extends BaseBackActivity {
+    @BindView(R.id.rv_setting)
+    RecyclerView mRvSetting;
+
     @Override
     protected int getLayoutId() {
         return R.layout.lock_ble_activity_setting;
     }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        setRvSetting();
+    }
+
+
+    private SettingAdapter mSettingAdapter;
+
+    private void setRvSetting() {
+        mSettingAdapter = new SettingAdapter(null);
+        mRvSetting.setLayoutManager(new LinearLayoutManager(getContext()) {
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        });
+        mRvSetting.setAdapter(mSettingAdapter);
+        mSettingAdapter.setOnItemClickListener((adapter, view, position) -> {
+            SettingInfo settingInfo = mSettingAdapter.getData().get(position);
+            switch (position) {
+                case 1:
+                    startActivity(new Intent(this,DeviceInfoActivity.class));
+                    break;
+                case 2:
+                    Intent intent = new Intent(this, EditDeviceNameActivity.class);
+                    intent.putExtra("name", settingInfo.getValue());
+                    startActivity(intent);
+                    break;
+            }
+        });
+        CommonUtils.setItemDivider(getContext(), mRvSetting);
+        List<SettingInfo> settingInfos = new ArrayList<>();
+        settingInfos.add(new SettingInfo("报警管理", ""));
+        settingInfos.add(new SettingInfo("设备信息", ""));
+        settingInfos.add(new SettingInfo("设备名称", "智能门锁09"));
+        settingInfos.add(new SettingInfo("安全设置", ""));
+        settingInfos.add(new SettingInfo("固件升级", ""));
+        settingInfos.add(new SettingInfo("帮助与反馈", ""));
+        mSettingAdapter.setNewInstance(settingInfos);
+        mSettingAdapter.setHeaderView(new SettingSoundView(this));
+    }
+
 }
