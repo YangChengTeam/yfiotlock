9fabe4e02076b5529f637163bf2288229ac1f97d
mzpbvsig <972403325@qq.com>
2021-03-05 14:47:09 +0800

    add fingerprint add pages

---
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/FingerprintAddSelectHandNextOpenLockActivity.java
@@ -0,0 +1,11 @@
+package com.yc.yfiotlock.controller.activitys.lock.ble;
+
+import com.yc.yfiotlock.R;
+import com.yc.yfiotlock.controller.activitys.base.BaseBackActivity;
+
+public class FingerprintAddSelectHandNextOpenLockActivity extends BaseBackActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.lock_ble_activity_fingerprint_add_select_hand_next_open_lock;
+    }
+}
