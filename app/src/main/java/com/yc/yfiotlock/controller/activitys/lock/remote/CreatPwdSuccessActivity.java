383228a827dd57c2f6b88d540c8ab457d84978b4
ckun9393 <caokun9393@163.com>
2021-03-05 14:15:03 +0800

    完善页面创建密码成功

---
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/remote/CreatPwdSuccessActivity.java
@@ -0,0 +1,34 @@
+package com.yc.yfiotlock.controller.activitys.lock.remote;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.yc.yfiotlock.R;
+import com.yc.yfiotlock.controller.activitys.base.BaseActivity;
+import com.yc.yfiotlock.model.bean.PassWordInfo;
+import com.yc.yfiotlock.view.widgets.BackNavBar;
+
+import butterknife.BindView;
+
+public class CreatPwdSuccessActivity extends BaseActivity {
+
+    @BindView(R.id.bnb_title)
+    BackNavBar mBnbTitle;
+
+    public static void start(Context context, PassWordInfo passWordInfo) {
+        Intent intent = new Intent(context, CreatPwdSuccessActivity.class);
+        intent.putExtra("password_info", passWordInfo);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_creat_pwd_success;
+    }
+
+    @Override
+    protected void initViews() {
+        mBnbTitle.setBackListener(view -> onBackPressed());
+
+    }
+}
