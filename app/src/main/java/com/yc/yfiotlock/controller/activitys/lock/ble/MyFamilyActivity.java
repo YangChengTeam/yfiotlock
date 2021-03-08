2bbd9f686d4074ad53bec0fa48bd1de0fce52c65
ckun9393 <caokun9393@163.com>
2021-03-08 12:04:34 +0800

    完善页面，房间管理

--- app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/MyFamilyActivity.java
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/MyFamilyActivity.java
@@ -7,15 +7,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemChildClickListener;
 import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.yc.yfiotlock.R;
 import com.yc.yfiotlock.controller.activitys.base.BaseActivity;
-import com.yc.yfiotlock.controller.activitys.lock.remote.TempPasswordOpenLockActivity;
-import com.yc.yfiotlock.controller.activitys.lock.remote.TempPwdDetailActivity;
 import com.yc.yfiotlock.model.bean.FamilyInfo;
-import com.yc.yfiotlock.model.bean.PassWordInfo;
 import com.yc.yfiotlock.view.adapters.MyFamilyAdapter;
-import com.yc.yfiotlock.view.adapters.TempPwdAdapter;
 import com.yc.yfiotlock.view.widgets.BackNavBar;
 
 import org.jetbrains.annotations.NotNull;
@@ -29,7 +26,7 @@ public class MyFamilyActivity extends BaseActivity {
 
     @BindView(R.id.bnb_title)
     BackNavBar mBnbTitle;
-    @BindView(R.id.rv_temp_pwd)
+    @BindView(R.id.rv_my_family)
     RecyclerView recyclerView;
     private MyFamilyAdapter myFamilyAdapter;
 
@@ -54,7 +51,20 @@ public class MyFamilyActivity extends BaseActivity {
         myFamilyAdapter.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(@NonNull @NotNull BaseQuickAdapter<?, ?> adapter, @NonNull @NotNull View view, int position) {
-//                TempPwdDetailActivity.start(MyFamilyActivity.this, myFamilyAdapter.getItem(position));
+                MyFamilyAddActivity.start(MyFamilyActivity.this, myFamilyAdapter.getItem(position));
+            }
+        });
+
+        myFamilyAdapter.setOnItemChildClickListener(new OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(@NonNull @NotNull BaseQuickAdapter adapter, @NonNull @NotNull View view, int position) {
+                switch (view.getId()) {
+                    case R.id.iv_family_number_default:
+                    case R.id.tv_family_number_default:
+                        FamilyInfo familyInfo = myFamilyAdapter.getData().get(position);
+                        myFamilyAdapter.updateCheck(familyInfo);
+                        break;
+                }
             }
         });
     }
--- app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/MyFamilyAddActivity.java
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/MyFamilyAddActivity.java
@@ -1,12 +1,25 @@
 package com.yc.yfiotlock.controller.activitys.lock.ble;
 
+import android.content.Context;
+import android.content.Intent;
+
 import com.yc.yfiotlock.R;
 import com.yc.yfiotlock.controller.activitys.base.BaseActivity;
+import com.yc.yfiotlock.controller.activitys.lock.remote.TempPwdDetailActivity;
+import com.yc.yfiotlock.model.bean.FamilyInfo;
+import com.yc.yfiotlock.model.bean.PassWordInfo;
 
 public class MyFamilyAddActivity extends BaseActivity {
+
+    public static void start(Context context, FamilyInfo familyInfo) {
+        Intent intent = new Intent(context, MyFamilyAddActivity.class);
+        intent.putExtra("family_info", familyInfo);
+        context.startActivity(intent);
+    }
+
     @Override
     protected int getLayoutId() {
-        return R.layout.lock_ble_activity_my_family;
+        return R.layout.lock_ble_activity_my_family_add;
     }
 
     @Override
