aa4843bd14af9f6589dc1fb812b5878cccd7da92
Dullyoung <664846453@qq.com>
2021-03-18 17:39:17 +0800

    finish ScanDeviceActivity UI finish QaActivity finish DeviceListActivity UI

--- app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/add/QaActivity.java
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/add/QaActivity.java
@@ -1,9 +1,9 @@
 package com.yc.yfiotlock.controller.activitys.lock.ble.add;
 
 import com.yc.yfiotlock.R;
-import com.yc.yfiotlock.controller.activitys.base.BaseActivity;
+import com.yc.yfiotlock.controller.activitys.base.BaseBackActivity;
 
-public class QaActivity  extends BaseActivity {
+public class QaActivity  extends BaseBackActivity {
     @Override
     protected int getLayoutId() {
         return R.layout.lock_ble_add_activity_qa;
--- app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/add/ScanDeviceActivity.java
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/add/ScanDeviceActivity.java
@@ -2,33 +2,34 @@ package com.yc.yfiotlock.controller.activitys.lock.ble.add;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 import com.coorchice.library.SuperTextView;
+import com.kk.securityhttp.utils.VUiKit;
 import com.yc.yfiotlock.R;
-import com.yc.yfiotlock.compat.ToastCompat;
 import com.yc.yfiotlock.controller.activitys.base.BaseActivity;
-import com.yc.yfiotlock.model.bean.lock.ble.LockInfo;
-import com.yc.yfiotlock.utils.CommonUtil;
-import com.yc.yfiotlock.view.BaseExtendAdapter;
+import com.yc.yfiotlock.controller.activitys.base.BaseBackActivity;
+import com.yc.yfiotlock.utils.AnimatinUtil;
 import com.yc.yfiotlock.view.widgets.BackNavBar;
-
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ScanDeviceActivity extends BaseActivity {
+public class ScanDeviceActivity extends BaseBackActivity {
 
+
+    @BindView(R.id.iv_scan_bg)
+    ImageView mIvScanBg;
+    @BindView(R.id.iv_scan_flag)
+    ImageView mIvScanFlag;
+    @BindView(R.id.tv_scan_state)
+    TextView mTvScanState;
+    @BindView(R.id.tv_scan_qa)
+    TextView mTvScanQa;
+    @BindView(R.id.stv_rescan)
+    SuperTextView mStvRescan;
 
     @Override
     protected int getLayoutId() {
@@ -37,8 +38,41 @@ public class ScanDeviceActivity extends BaseActivity {
 
     @Override
     protected void initViews() {
-        startActivity(new Intent(this, DeviceListActivity.class));
+        super.initViews();
+
     }
 
+    @Override
+    protected void bindClick() {
+        setClick(mStvRescan, () -> {
+            setScanStartUi("开始扫描", "扫描不到怎么办？");
+            VUiKit.postDelayed(6000, () -> {
+                setScanStopUi("未发现附近有可添加设备", "无法扫到设备怎么办？");
+            });
+        });
+        setClick(mTvScanQa, () -> startActivity(new Intent(this, QaActivity.class)));
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setScanStartUi("开始扫描", "扫描不到怎么办？");
+        VUiKit.postDelayed(6000, () -> {
+            setScanStopUi("未发现附近有可添加设备", "无法扫到设备怎么办？");
+        });
+    }
+
+    private void setScanStartUi(String title, String qa) {
+        AnimatinUtil.rotate(mIvScanFlag);
+        mStvRescan.setVisibility(View.GONE);
+        mTvScanState.setText(title);
+        mTvScanQa.setText(qa);
+    }
+
+    private void setScanStopUi(String title, String qa) {
+        mIvScanFlag.clearAnimation();
+        mStvRescan.setVisibility(View.VISIBLE);
+        mTvScanState.setText(title);
+        mTvScanQa.setText(qa);
+    }
 }
