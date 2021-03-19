e24951198592f61ff1deac410f81c4e72c42d480
Dullyoung <664846453@qq.com>
2021-03-19 14:46:46 +0800

    finish connect about ui add kt support

--- app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/add/Connect2Activity.java
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/add/Connect2Activity.java
@@ -1,13 +1,36 @@
 package com.yc.yfiotlock.controller.activitys.lock.ble.add;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.yc.yfiotlock.R;
 import com.yc.yfiotlock.controller.activitys.base.BaseBackActivity;
+import com.yc.yfiotlock.controller.dialogs.lock.ble.ChangeDeviceNameDialog;
 import com.yc.yfiotlock.libs.fastble.data.BleDevice;
+import com.yc.yfiotlock.view.widgets.CircularProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class Connect2Activity extends BaseBackActivity {
+
+    @BindView(R.id.cpb_progress)
+    CircularProgressBar mCpbProgress;
+    @BindView(R.id.tv_progress)
+    TextView mTvProgress;
+    @BindView(R.id.ll_connecting)
+    LinearLayout mLlConnecting;
+    @BindView(R.id.tv_edit)
+    TextView mTvEdit;
+    @BindView(R.id.ll_connected)
+    LinearLayout mLlConnected;
+
     @Override
     protected int getLayoutId() {
         return R.layout.lock_ble_activity_add_connect2;
@@ -19,8 +42,67 @@ public class Connect2Activity extends BaseBackActivity {
         context.startActivity(intent);
     }
 
+    private String deviceName = "";
+
     @Override
     protected void initViews() {
         super.initViews();
+        BleDevice bleDevice = getIntent().getParcelableExtra("bleDevice");
+        if (bleDevice != null) {
+            deviceName = bleDevice.getName();
+        }
+        backNavBar.setTitle(deviceName);
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0f, 100f);
+        valueAnimator.addUpdateListener(animation -> {
+            float a = (float) animation.getAnimatedValue();
+            mCpbProgress.setProgress(a);
+            mTvProgress.setText((int) a + "%");
+            if ((int) a == 100) {
+                onConnectFail();
+            } else {
+                showConnectingUi();
+            }
+        });
+        valueAnimator.setDuration(3000);
+        valueAnimator.start();
+
+    }
+
+    /**
+     * 连接失败的跳转
+     */
+    private void onConnectFail() {
+        Intent intent = new Intent(this, ConnectFailActivity.class);
+        intent.putExtra("name", deviceName);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * 连接中UI
+     */
+    private void showConnectingUi() {
+        mLlConnected.setVisibility(View.GONE);
+        mLlConnecting.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 连接成功UI
+     */
+    private void showConnectedUi() {
+        mLlConnected.setVisibility(View.VISIBLE);
+        mLlConnecting.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void bindClick() {
+        super.bindClick();
+        setClick(mTvEdit, () -> {
+            ChangeDeviceNameDialog deviceNameDialog = new ChangeDeviceNameDialog(this);
+            deviceNameDialog.setOnSureClick(name -> {
+
+            });
+            deviceNameDialog.show(mTvEdit.getText().toString());
+        });
     }
 }
--- app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/add/ConnectFailActivity.java
+++ app/src/main/java/com/yc/yfiotlock/controller/activitys/lock/ble/add/ConnectFailActivity.java
@@ -1,5 +1,37 @@
 package com.yc.yfiotlock.controller.activitys.lock.ble.add;
 
-public class ConnectFailActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.coorchice.library.SuperTextView;
+import com.yc.yfiotlock.R;
+import com.yc.yfiotlock.controller.activitys.base.BaseBackActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ConnectFailActivity extends BaseBackActivity {
+
+    @BindView(R.id.iv_pic)
+    ImageView mIvPic;
+    @BindView(R.id.ll_tip)
+    LinearLayout mLlTip;
+    @BindView(R.id.stv_reset)
+    SuperTextView mStvReset;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.lock_ble_add_activity_connect_fail;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        String name = getIntent().getStringExtra("name") == null ? "" : getIntent().getStringExtra("name");
+        backNavBar.setTitle(name);
+        setClick(mStvReset, this::finish);
+    }
+
 
 }
